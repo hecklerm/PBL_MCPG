370cbeb24a3e762ac155cfddaa2783a985f4dbfb
Mark Heckler <mark.heckler@gmail.com>
2016-07-02 00:31:29 -0300

    Minor cleanup.

--- src/app.js
+++ src/app.js
@@ -525,13 +525,6 @@ webSocketData.onmessage = function(event) {
   rLabel.text(rainfall + RLABEL);
   pLabel.text(pressure + PLABEL);
   
-//   // Show to user
-//   main.subtitle('Latest Reading');
-//   main.body('T: ' + temp + ' H: ' + hum + '\n' +
-//             'V: ' + volts + ' L: ' + lum + '\n' +
-//             'WDir: ' + windDir + ' Spd: ' + windSpeed + '\n' +
-//             'Rain: ' + rainfall + ' Prs: ' + pressure + '\n' + 
-//             'Status: ' + status);
   // Populate readingStatus object with current values for physical interfaces
   populateStatus(status);
 };
@@ -640,50 +633,3 @@ webSocketData.onmessage = function(event) {
 //   webSocketCtrl = new WebSocket(serverMCP + "/control");
 // };
 
-
-
-
-// main.on('click', 'select', function(e) {
-//   var wind = new UI.Window({
-//     backgroundColor: 'black'
-//   });
-//   var radial = new UI.Radial({
-//     size: new Vector2(140, 140),
-//     angle: 0,
-//     angle2: 300,
-//     radius: 20,
-//     backgroundColor: 'cyan',
-//     borderColor: 'celeste',
-//     borderWidth: 1,
-//   });
-//   var textfield = new UI.Text({
-//     size: new Vector2(140, 60),
-//     font: 'gothic-24-bold',
-//     text: 'Dynamic\nWindow',
-//     textAlign: 'center'
-//   });
-//   var windSize = wind.size();
-//   // Center the radial in the window
-//   var radialPos = radial.position()
-//       .addSelf(windSize)
-//       .subSelf(radial.size())
-//       .multiplyScalar(0.5);
-//   radial.position(radialPos);
-//   // Center the textfield in the window
-//   var textfieldPos = textfield.position()
-//       .addSelf(windSize)
-//       .subSelf(textfield.size())
-//       .multiplyScalar(0.5);
-//   textfield.position(textfieldPos);
-//   wind.add(radial);
-//   wind.add(textfield);
-//   wind.show();
-// });
-
-// main.on('click', 'down', function(e) {
-//   var card = new UI.Card();
-//   card.title('A Card');
-//   card.subtitle('Is a Window');
-//   card.body('The simplest window type in Pebble.js.');
-//   card.show();
-// });
